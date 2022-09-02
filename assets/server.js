480f02ac900680958bf1bb704f597ad1e91bcb31
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-03 01:05:19 +0300

    will fetch request

--- assets/server.js
+++ assets/server.js
@@ -1,10 +1,10 @@
 
 
-// const apiUrl="http://localhost:3000/suggestions"
+const apiUrl="http://localhost:3000/suggestions"
 
-// function getMorningSuggestion(id){
-//     fetch(`${apiUrl}/${id}`).then ((response) => response.json()).then((data) =>{
-//         document.querySelectorAll("#breakfast ul").innerHTML = data.morning
-
-//     })
-// }
+function getMorningSuggestion(id){
+    fetch(`${apiUrl}/${id}`).then ((response) => response.json()).then((data) =>{
+        document.querySelectorAll("#breakfast ul").innerHTML = data.morning
+        
+    })
+}
