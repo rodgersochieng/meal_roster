c512bcfd5e3f15dec7550256b6fa7173d2e8e5f3
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-05 17:12:45 +0300

    updating json

--- assets/server.js
+++ assets/server.js
@@ -1,6 +1,7 @@
 let array = []
 let array2 = []
 let array3 = []
+let array4 = []
  
  fetch ("http://localhost:3000/suggestions")
  .then(response => response.json())
@@ -15,6 +16,7 @@ display(data)
         array.push(item.morning)
         array2.push(item.dayMeal)
         array3.push(item.nightMeal)
+        array4.push(item.healthyTrics)
         // console.log(array)
         
 
@@ -24,6 +26,7 @@ display(data)
         document.querySelector('#breakfast li').innerHTML = array
         document.querySelector('#lunch li').innerHTML = array2
         document.querySelector('#dinner li').innerHTML = array3
+        document.querySelector('#eating-habbits li').innerHTML += array4
   
     } )
     
