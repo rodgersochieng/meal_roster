5f8188ca5af840d81676a1896722332632830472
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-05 16:47:43 +0300

    updating json

--- assets/server.js
+++ assets/server.js
@@ -1,10 +1,35 @@
+let array = []
+let array2 = []
+let array3 = []
+ 
+ fetch ("http://localhost:3000/suggestions")
+ .then(response => response.json())
+ .then(data => {
 
+display(data)
+ })
 
-const apiUrl="http://localhost:3000/suggestions"
+ function display(data){
 
-function getMorningSuggestion(id){
-    fetch(`${apiUrl}/${id}`).then ((response) => response.json()).then((data) =>{
-        document.querySelectorAll("#breakfast ul").innerHTML = data.morning
+    for(let item of data){ 
+        array.push(item.morning)
+        array2.push(item.dayMeal)
+        array3.push(item.nightMeal)
+        // console.log(array)
         
-    })
-}
+
+    }
+    const btn = document.getElementById('first-button')
+    btn.addEventListener('click', () => {
+        document.querySelector('#breakfast li').innerHTML = array
+        document.querySelector('#lunch li').innerHTML = array2
+        document.querySelector('#dinner li').innerHTML = array3
+  
+    } )
+    
+    
+ }
+
+//  function display(data) {}
+
+
